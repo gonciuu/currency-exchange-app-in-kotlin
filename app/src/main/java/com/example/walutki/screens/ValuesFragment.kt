c89f7b5f0b09d7c66fb76bfaf201d4dda8744639
andryue <kacperwojak17@gmail.com>
2020-07-09 09:46:55 +0200

    add navigation to app

--- app/src/main/java/com/example/walutki/screens/ValuesFragment.kt
+++ app/src/main/java/com/example/walutki/screens/ValuesFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.walutki.R
+import kotlinx.android.synthetic.main.fragment_values.*
 
 
 class ValuesFragment : Fragment() {
@@ -17,6 +19,14 @@ class ValuesFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_values, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        click.setOnClickListener {
+            findNavController().navigate(R.id.action_valuesFragment_to_calculateFragment)
+        }
     }
 
 }
