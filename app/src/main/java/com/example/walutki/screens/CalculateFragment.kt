1676d8601c9d869769c344cfa38c591c098a4ad1
andryue <kacperwojak17@gmail.com>
2020-07-10 10:28:25 +0200

    create layout wifeframe

--- app/src/main/java/com/example/walutki/screens/CalculateFragment.kt
+++ app/src/main/java/com/example/walutki/screens/CalculateFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.walutki.R
+import com.example.walutki.screens.adapters.CurrentiesAdapter
+import kotlinx.android.synthetic.main.fragment_calculate.*
 
 
 class CalculateFragment : Fragment() {
@@ -16,5 +19,14 @@ class CalculateFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_calculate, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        anotherValuesRecyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = CurrentiesAdapter()
+        }
     }
 }
