347ed0f69af10861082bb9105b98ef19a53d2aca
andryue <kacperwojak17@gmail.com>
2020-07-12 12:58:21 +0200

    convert currencies object to hashmap which contains the most popular currencies

--- app/src/main/java/com/example/walutki/screens/ValuesFragment.kt
+++ app/src/main/java/com/example/walutki/screens/ValuesFragment.kt
@@ -1,19 +1,24 @@
 package com.example.walutki.screens
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.walutki.R
 import com.example.walutki.screens.adapters.CurrentiesAdapter
+import com.example.walutki.screens.view_models.LoadingViewModel
 import kotlinx.android.synthetic.main.fragment_values.*
 
 
 class ValuesFragment : Fragment() {
 
+    private lateinit var loadingViewModel:LoadingViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_values, container, false)
@@ -21,13 +26,26 @@ class ValuesFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        loadingViewModel = ViewModelProvider(requireActivity()).get(LoadingViewModel::class.java)
 
         calculateButton.setOnClickListener {
             findNavController().navigate(R.id.action_valuesFragment_to_calculateFragment)
         }
 
+        getCurrencies()
+
+    }
 
 
+    private fun getCurrencies(){
+        loadingViewModel.getCurrency().observe(viewLifecycleOwner, Observer {
+            Log.d("ALERT",it["PLN"].toString())
+        })
+        setAdapter()
+    }
+
+
+    private fun setAdapter(){
         currentRecyclerView.apply {
             layoutManager = LinearLayoutManager(context)
             adapter = CurrentiesAdapter()
--- app/src/main/java/com/example/walutki/screens/view_models/LoadingViewModel.kt
+++ app/src/main/java/com/example/walutki/screens/view_models/LoadingViewModel.kt
@@ -6,11 +6,27 @@ import androidx.lifecycle.ViewModel
 import com.example.walutki.currencies_from_api.obj_class.Currency
 
 class LoadingViewModel : ViewModel() {
-    private val currency = MutableLiveData<Currency>()
+    private val currency = MutableLiveData<HashMap<String,Double>>()
 
     fun setCurrency(userCurrency: Currency){
-        currency.value = userCurrency
+        currency.value = setCurrencies(userCurrency)
     }
 
-    fun getCurrency() :LiveData<Currency> = currency
+    fun getCurrency() :LiveData<HashMap<String,Double>> = currency
+
+
+    private fun setCurrencies(userCurrency: Currency) : HashMap<String,Double>{
+        val map = HashMap<String,Double>()
+        map["PLN"] = userCurrency.rates.pLN
+        map["EUR"] = userCurrency.rates.eUR
+        map["USD"] = userCurrency.rates.uSD
+        map["HRK"] = userCurrency.rates.hRK //kuna horwacka
+        map["JPY"] = userCurrency.rates.jPY //jen japonski
+        map["GBP"] = userCurrency.rates.gBP //funt
+        map["AUD"] = userCurrency.rates.aUD //dolar australijski
+        map["CHF"] = userCurrency.rates.cHF //frank
+        map["CAD"] = userCurrency.rates.cAD //dalar kanadysjki
+        map["CZK"] = userCurrency.rates.cZK //korona czeska
+        return  map
+    }
 }
