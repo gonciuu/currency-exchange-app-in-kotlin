67735c601cb49c2a91ff862f5ba1b17b4ce021da
andryue <kacperwojak17@gmail.com>
2020-07-13 12:31:17 +0200

    create adapters to spinners and set on click on spinner items

--- app/src/main/java/com/example/walutki/screens/CalculateFragment.kt
+++ app/src/main/java/com/example/walutki/screens/CalculateFragment.kt
@@ -1,31 +1,80 @@
 package com.example.walutki.screens
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
+import android.widget.ArrayAdapter
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.walutki.R
-import com.example.walutki.screens.adapters.CurrenciesAdapter
+import com.example.walutki.screens.view_models.LoadingViewModel
 import kotlinx.android.synthetic.main.fragment_calculate.*
 
 
 class CalculateFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    private lateinit var loadingViewModel: LoadingViewModel
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_calculate, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        loadingViewModel = ViewModelProvider(requireActivity()).get(LoadingViewModel::class.java)
+
+        getCurrencies()
+
 
         anotherValuesRecyclerView.apply {
             layoutManager = LinearLayoutManager(context)
         }
     }
+
+
+    private fun getCurrencies(){
+        loadingViewModel.getCurrency().observe(viewLifecycleOwner, Observer {
+            setSpinners(it)
+        })
+    }
+
+
+
+
+    private fun setSpinners(currencies : HashMap<String,Double>){
+        val firstCurrentAdapter = ArrayAdapter(requireContext(),android.R.layout.simple_spinner_dropdown_item,currencies.keys.toList())
+        firstCurrentSpinner.adapter = firstCurrentAdapter
+
+        val secondCurrentAdapter = ArrayAdapter(requireContext(),android.R.layout.simple_spinner_dropdown_item,currencies.keys.toList())
+        secondCurrentSpinner.adapter = secondCurrentAdapter
+
+        setSpinnersOnClick()
+    }
+
+
+    private fun setSpinnersOnClick(){
+        firstCurrentSpinner.onItemSelectedListener = object : OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                val selectedItem = parent.getItemAtPosition(position).toString()
+                Log.d("ALERT",selectedItem)
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        }
+        secondCurrentSpinner.onItemSelectedListener = object : OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                val selectedItem = parent.getItemAtPosition(position).toString()
+                Log.d("ALERT",selectedItem)
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        }
+    }
+
 }
