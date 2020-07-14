552a2899a79a19a9773119d10f1119397d5e3751
andryue <kacperwojak17@gmail.com>
2020-07-14 10:16:59 +0200

    set main currency value and today date

--- app/src/main/java/com/example/walutki/screens/CalculateFragment.kt
+++ app/src/main/java/com/example/walutki/screens/CalculateFragment.kt
@@ -5,10 +5,8 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
+import android.widget.*
 import android.widget.AdapterView.OnItemSelectedListener
-import android.widget.ArrayAdapter
-import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -17,6 +15,10 @@ import com.example.walutki.R
 import com.example.walutki.screens.view_models.LoadingViewModel
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.fragment_calculate.*
+import java.lang.NumberFormatException
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.HashMap
 
 
 class CalculateFragment : Fragment() {
@@ -36,11 +38,8 @@ class CalculateFragment : Fragment() {
         loadingViewModel = ViewModelProvider(requireActivity()).get(LoadingViewModel::class.java)
 
         getCurrencies()
+        setDate()
 
-
-        anotherValuesRecyclerView.apply {
-            layoutManager = LinearLayoutManager(context)
-        }
     }
 
 
@@ -59,40 +58,28 @@ class CalculateFragment : Fragment() {
         )
         firstCurrentSpinner.adapter = firstCurrentAdapter
 
-        val secondCurrentAdapter = ArrayAdapter(
-            requireContext(),
-            android.R.layout.simple_spinner_dropdown_item,
-            currencies.keys.toList()
-        )
+        val secondCurrentAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_dropdown_item, currencies.keys.toList())
         secondCurrentSpinner.adapter = secondCurrentAdapter
 
-        setSpinnersOnClick()
+        setSpinnersOnClick(currencies)
     }
 
 
-    private fun setSpinnersOnClick() {
+    private fun setSpinnersOnClick(currencies: HashMap<String, Double>) {
         firstCurrentSpinner.onItemSelectedListener = object : OnItemSelectedListener {
-            override fun onItemSelected(
-                parent: AdapterView<*>,
-                view: View,
-                position: Int,
-                id: Long
-            ) {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
                 val selectedItem = parent.getItemAtPosition(position).toString()
                 setFlagImage(selectedItem, firstCurrentFlag)
+                setCurrentValue(currencies[selectedItem]!!,currencies[secondCurrentSpinner.selectedItem.toString()]!!)
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {}
         }
         secondCurrentSpinner.onItemSelectedListener = object : OnItemSelectedListener {
-            override fun onItemSelected(
-                parent: AdapterView<*>,
-                view: View,
-                position: Int,
-                id: Long
-            ) {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
                 val selectedItem = parent.getItemAtPosition(position).toString()
                 setFlagImage(selectedItem, secondCurrentFlag)
+                setCurrentValue(currencies[firstCurrentSpinner.selectedItem.toString()]!!,currencies[selectedItem]!!)
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {}
@@ -103,6 +90,20 @@ class CalculateFragment : Fragment() {
     private fun setFlagImage(countryCode: String, imageView: ImageView) = Picasso.get()
         .load("https://www.countryflags.io/${countryCode[0] + "" + countryCode[1]}/flat/64.png")
         .fit().centerCrop().into(imageView)
+
+
+    private fun setDate(){
+        dateTV.text =  SimpleDateFormat("dd MMMM yyyy", Locale.ENGLISH).format(Calendar.getInstance().timeInMillis)
+    }
+
+
+    private fun setCurrentValue(firstSpinnerValue:Double,secondSpinnerValue:Double){
+        try{
+            currentValue.text = String.format("%.4f",secondSpinnerValue / firstSpinnerValue)
+        }catch (nbEx:NumberFormatException){
+            Toast.makeText(context,"Number firmat exception handled",Toast.LENGTH_LONG).show()
+        }
+    }
 
 
 }
